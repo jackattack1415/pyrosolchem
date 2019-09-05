56c8a86e79af414055cd5505fcc9ca102c44b304
jackattack1415 <jhensley@g.harvard.edu>
2019-09-05 11:20:31 -0400

    load_components and load_constants functions added

--- src/d00_utils/loading_utils.py
+++ src/d00_utils/loading_utils.py
@@ -1,5 +1,5 @@
 import os
-
+import yaml
 
 def get_project_directory():
     """ Returns project directory.
@@ -7,6 +7,37 @@ def get_project_directory():
         ---
         project_directory (str): path back to top level directory
     """
+
     project_directory = os.path.dirname(os.path.realpath(__file__))
 
     return project_directory
+
+
+def load_components():
+    """ Returns components as a dictionary.
+        Outputs
+        ---
+        components (dict): definitions of components in solution.
+    """
+
+    project_directory = get_project_directory()
+    filepath = os.sep.join(project_directory + 'conf/components.yml'.split('/'))
+    with open(filepath) as f:
+        components = yaml.safe_load(f)
+
+    return components
+
+
+def load_constants():
+    """ Returns constants.
+        Outputs
+        ---
+        project_directory (str): path back to top level directory
+    """
+
+    project_directory = get_project_directory()
+    filepath = os.sep.join(project_directory + 'conf/constants.yml'.split('/'))
+    with open(filepath) as f:
+        constants = yaml.safe_load(f)
+
+    return constants
