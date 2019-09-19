adfe0cf011cba28578ccb3bb68dc82e3c0d0eb57
jackattack1415 <jhensley@g.harvard.edu>
2019-09-19 14:38:00 -0400

    added functions to save filtered ata

--- src/d00_utils/data_utils.py
+++ src/d00_utils/data_utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from datetime import date
 
 from src.d00_utils.conf_utils import get_project_directory
 
@@ -34,3 +35,46 @@ def add_normalized_intensity_column(df, internal_std='p283'):
         df['n' + p_col[1:]] = df[p_col] / df[internal_std]
 
     return df
+
+
+def filter_ms_data_in_experiments(df, experiment_parameters):
+    """
+    """
+
+    df_filtered = pd.DataFrame()
+    for experiment_name, experiment in experiment_parameters.items():
+        query_parts = []
+        query_parts.append("comp == '{}'".format(experiment['solution_name']))
+        query_parts.append("trapped>={} and trapped<{}".format(*experiment['trap_time']))
+
+        if experiment['other_query'] is not None:
+            query_parts.append(experiment['other_query'])
+
+        query = " and ".join(query_parts)
+
+        df_experiment = (df.query(query).
+                             loc[experiment['idx_range'][0]:experiment['idx_range'][1]])
+        if experiment['bad_idx'] is not None:
+            df_experiment = df_experiment.drop(experiment['bad_idx'])
+
+        df_filtered = df_filtered.append(df_experiment)
+
+    return df_filtered
+
+
+def save_data_frame(df_to_save, raw_data_path, level_of_cleaning):
+    """
+
+    :param df_to_save:
+    :param raw_data_path:
+    :param level_of_cleaning:
+    :return:
+    """
+
+    project_dir = get_project_directory()
+    today_str = date.today().strftime("%Y%m%d")
+
+    file_name = today_str[2:] + '-' + raw_data_path.split('-')[1:] + '-' + level_of_cleaning + '.csv'
+    file_path = os.path.join(project_dir, 'data', file_name)
+
+    df_to_save.to_csv(path_or_buf=file_path, index=False)
