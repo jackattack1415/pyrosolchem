d52cea8b3afa2445944e70fd34c6c661cd1b00df
jackattack1415 <jhensley@g.harvard.edu>
2019-09-26 09:27:41 -0400

    made these functions more generalizable

--- src/d05_reporting/plot_ms_data.py
+++ src/d05_reporting/plot_ms_data.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from src.d00_utils.conf_utils import get_project_directory
 
 
-def plot_ms_data(df, experiment_names, t_col_name, y_col_name, y_axis_label):
+def plot_ms_data(df, experiment_names, t_col_name, y_col_name, analyte=None, internal_standard=None):
     """
     """
 
@@ -29,14 +29,25 @@ def plot_ms_data(df, experiment_names, t_col_name, y_col_name, y_axis_label):
 
         ax = sns.scatterplot(x=xs, y=ys, label=experiment_name, alpha=1, s=200)
 
+    if y_col_name[:2] == 'mz':
+        mzs = y_col_name.split('_')
+        y_axis_label = 'relative ms intensities \n (%s %s$^{-1}$)' % (mzs[0], mzs[1])
+        fig_type = 'rel_ms'
+    elif y_col_name[:2] == 'mo' and y_col_name[-2:] != '_0':
+        y_axis_label = 'relative molar abundance \n (mol$_{%s}$ / mol$_{%s}$)' % (analyte, internal_standard)
+        fig_type = 'rel_mol'
+    elif y_col_name[-2:] == '_0':
+        y_axis_label = 'relative molar abundance \n (mol$_{%s}$ / mol$_{%s,\!0}$)' % (analyte, analyte)
+        fig_type = 'decay'
+
     ax.set(xlabel='time (%s)' % t_col_name, ylabel=y_axis_label,
-           ylim=(-y_max * 0.1, y_max * 1.1), xlim=(-x_max * 0.05, x_max * 1.05))
+           ylim=(-y_max * 0.05, y_max * 1.5), xlim=(-x_max * 0.05, x_max * 1.05))
     ax.legend(title='Experiment')
 
     project_dir = get_project_directory()
     today_str = date.today().strftime("%Y%m%d")
     expt_strings = '-'.join(experiment_names)
-    file_name = today_str[2:] + '-' + expt_strings + '.png'
+    file_name = today_str[2:] + '-' + fig_type + '-' + expt_strings + '.png'
     file_path = os.path.join(project_dir, 'results', 'figs_out', file_name)
 
     plt.savefig(file_path, bbox_inches='tight', dpi=300, transparent=True)
