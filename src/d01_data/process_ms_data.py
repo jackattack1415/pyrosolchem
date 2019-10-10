d782ca891a38f5cf95f6899ef03a88d7e505c14a
jackattack1415 <jhensley@g.harvard.edu>
2019-10-10 14:09:20 -0400

    changed experiments in processed data

--- src/d01_data/process_ms_data.py
+++ src/d01_data/process_ms_data.py
@@ -1,54 +1,99 @@
 import pandas as pd
 
 from src.d00_utils.processing_utils import get_bootstrapped_statistics
-from src.d00_utils.data_utils import extract_calibration_data
+from src.d00_utils.data_utils import extract_calibration_data, save_data_frame, import_ms_data
 
 
-def process_ms_data_in_evap_experiments(df_cleaned, experiments):
+def process_ms_data_in_evap_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
     """
     """
 
+    experiment_name = [*experiments_dict].pop()
+    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
+                                directory=experiment_name)
     df_processed = df_cleaned.copy(deep=True)
     df_processed = add_calibrated_ms_data_columns(df=df_processed,
-                                                  experiments=experiments,
+                                                  experiments=experiments_dict,
                                                   analyte='Butenedial',
                                                   internal_standard='PEG-6')
 
-    df_processed.rename(columns={'mins': 'hrs'})
-    df_processed.hrs = df_cleaned.hrs / 60
+    df_processed = df_processed.rename(columns={'mins': 'hrs'})
+    df_processed.hrs = df_processed.hrs / 60
+
+    if save_processed_data:
+        save_data_frame(df_to_save=df_processed,
+                        experiment_label=experiment_name,
+                        level_of_cleaning='PROCESSED')
 
     return df_processed
 
 
-def process_ms_data_in_droplet_vs_vial_experiments(df_cleaned):
+def process_ms_data_in_droplet_vs_vial_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
     """
     """
 
+    experiment_name = [*experiments_dict].pop()
+    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
+                                directory=experiment_name)
     df_processed = df_cleaned.copy(deep=True)
     df_processed['hrs'] = (df_processed.mins + df_processed.vial) / 60
-    df_processed.drop(['vial', 'mins'], axis=1)
+    df_processed = df_processed.drop(['vial', 'mins'], axis=1)
+
+    if save_processed_data:
+        save_data_frame(df_to_save=df_processed,
+                        experiment_label=experiment_name,
+                        level_of_cleaning='PROCESSED')
 
     return df_processed
 
 
-def process_ms_data_in_nh3g_experiments(df_cleaned):
+def process_ms_data_in_pyrrolinone_evap_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
     """
     """
 
+    experiment_name = [*experiments_dict].pop()
+    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
+                                directory=experiment_name)
     df_processed = df_cleaned.copy(deep=True)
-    df_processed['experiment'] = 'bd_nh3g_' + df_processed.nh3_bubbler.astype(str).str.replace('.','')
+
+    df_processed = df_processed.rename(columns={'mins': 'hrs', 'vial': 'hrs_in_vial'})
+    df_processed.hrs = df_processed.hrs / 60
+    df_processed.hrs_in_vial = df_processed.hrs_in_vial / 60
+    df_processed['experiment'] = "pyr_nh42so4"
+    df_processed = df_processed[df_processed.hrs < 3]
+
+    if save_processed_data:
+        save_data_frame(df_to_save=df_processed,
+                        experiment_label=experiment_name,
+                        level_of_cleaning='PROCESSED')
+
+    return df_processed
+
+
+def process_ms_data_in_nh3g_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
+    """
+    """
+    experiment_name = [*experiments_dict].pop()
+    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
+                                directory=experiment_name)
+    df_processed = df_cleaned.copy(deep=True)
+    df_processed['experiment'] = 'bd_nh3g_' + df_processed.nominal_nh3_mM.astype(str).str.replace('.','')
 
     # df_processed['mM_nhx']  add this to the droplet definitions soon
 
+    if save_processed_data:
+        save_data_frame(df_to_save=df_processed,
+                        experiment_label=experiment_name,
+                        level_of_cleaning='PROCESSED')
+
     return df_processed
 
 
-
-def add_calibrated_ms_data_columns(df, experiments, analyte, internal_standard='PEG-6'):
+def add_calibrated_ms_data_columns(df, experiments_dict, analyte, internal_standard='PEG-6'):
     """"""
 
     df_calibrated = pd.DataFrame()
-    for experiment_name, experiment_defs in experiments.items():
+    for experiment_name, experiment_defs in experiments_dict.items():
 
         df_experiment = df[df.experiment == experiment_name]
         ms_signal_inits = extract_calibration_data(df=df_experiment,
