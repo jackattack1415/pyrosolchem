4cfc4b97ce76be18742f9d8bde68e35b17b11458
jackattack1415 <jhensley@g.harvard.edu>
2019-10-15 18:34:36 -0400

    new saved file names

--- src/d00_utils/data_utils.py
+++ src/d00_utils/data_utils.py
@@ -5,18 +5,19 @@ from datetime import date
 from src.d00_utils.conf_utils import get_project_directory
 
 
-def import_ms_data(file_name, directory=None):
+def import_ms_data(file_name, subdirectory=None):
     """
 
     :param file_name:
-    :param directory:
+    :param subdirectory:
     :return:
     """
 
-    if directory is not None:
-        data_path = os.path.join(directory, file_name)
+    project_dir = get_project_directory()
+    if subdirectory is not None:
+        data_path = os.path.join(project_dir, 'data', subdirectory, file_name)
     else:
-        project_dir = get_project_directory()
+
         data_path = os.path.join(project_dir, 'data', file_name)
 
     df = pd.read_csv(data_path)
@@ -33,19 +34,23 @@ def extract_calibration_data(df, t_init_cutoff, cal_data_col):
     return ms_signal_inits
 
 
-def save_data_frame(df_to_save, raw_data_file_name, level_of_cleaning):
+def save_data_frame(df_to_save, experiment_label, level_of_cleaning):
     """
 
     :param df_to_save:
-    :param raw_data_file_name:
+    :param experiment_name:
     :param level_of_cleaning:
     :return:
     """
 
     project_dir = get_project_directory()
     today_str = date.today().strftime("%Y%m%d")
+    file_dir = os.path.join(project_dir, 'data', experiment_label)
 
-    file_name = today_str[2:] + '-' + raw_data_file_name.split('-')[1:] + '-' + level_of_cleaning + '.csv'
-    file_path = os.path.join(project_dir, 'data', file_name)
+    if not os.path.exists(file_dir):
+        os.mkdir(file_dir)
+
+    file_name = today_str[2:] + '-' + experiment_label + '-' + level_of_cleaning + '.csv'
+    file_path = os.path.join(file_dir, file_name)
 
     df_to_save.to_csv(path_or_buf=file_path, index=False)
