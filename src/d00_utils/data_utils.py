0bc6342024c2c012014acc1870b8038ea199838c
Jack Hensley <jack.chr.hensley@gmail.com>
2020-03-19 09:57:36 -0400

    updating data folderimports with raw and treated data subdirectories

--- src/d00_utils/data_utils.py
+++ src/d00_utils/data_utils.py
@@ -5,21 +5,30 @@ from datetime import date
 from src.d00_utils.conf_utils import get_project_directory
 
 
-def import_ms_data(file_name, subdirectory=None):
-    """
+def import_raw_csv_data(file_name):
+    """ Imports the csv file as a dataframe from the data_raw directory
 
-    :param file_name:
-    :param subdirectory:
-    :return:
+    :param file_name: str. Only the file name (not the path) of the raw csv file, located in data_raw folder.
+    :return: df. Contains the data embedded in the csv of the file name selected.
     """
 
     project_dir = get_project_directory()
-    if subdirectory is not None:
-        data_path = os.path.join(project_dir, 'data', subdirectory, file_name)
-    else:
+    data_path = os.path.join(project_dir, 'data', file_name)
+    df = pd.read_csv(data_path)
 
-        data_path = os.path.join(project_dir, 'data', file_name)
+    return df
 
+
+def import_cleaned_csv_data(file_name, experiment_label):
+    """
+
+    :param file_name: str. Only the file name (not the path) of the raw csv file, located in data_treated.
+    :param experiment_label: str. The experiment shorthand used additionally as the directory name in data_treated.
+    :return: df. Contains the data embedded in the csv of the file name selected.
+    """
+
+    project_dir = get_project_directory()
+    data_path = os.path.join(project_dir, 'data_treated', experiment_label, file_name)
     df = pd.read_csv(data_path)
 
     return df
@@ -34,13 +43,13 @@ def extract_calibration_data(df, t_init_cutoff, cal_data_col):
     return ms_signal_inits
 
 
-def save_data_frame(df_to_save, experiment_label, level_of_cleaning):
-    """
+def save_data_frame(df_to_save, experiment_label, level_of_treatment):
+    """ Saves cleaned data frame under experiment subdirectory in the data_cleaned directory.
 
-    :param df_to_save:
-    :param experiment_name:
-    :param level_of_cleaning:
-    :return:
+    :param df_to_save: df. Dataframe that will be saved.
+    :param experiment_label: str. Name of the experiment to which the dataframe belongs.
+    :param level_of_treatment: str. Level of cleaning done to df (FILTERED, PROCESSED, CLUSTERED, MODELED)
+    :return: None
     """
 
     project_dir = get_project_directory()
@@ -50,7 +59,7 @@ def save_data_frame(df_to_save, experiment_label, level_of_cleaning):
     if not os.path.exists(file_dir):
         os.mkdir(file_dir)
 
-    file_name = today_str[2:] + '-' + experiment_label + '-' + level_of_cleaning + '.csv'
+    file_name = today_str + '_' + experiment_label + '_' + level_of_treatment + '.csv'
     file_path = os.path.join(file_dir, file_name)
 
     df_to_save.to_csv(path_or_buf=file_path, index=False)
