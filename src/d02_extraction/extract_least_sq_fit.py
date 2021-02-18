ba414292a7fe0e51db44b1124fc64cf617e0c6cc
Jack Hensley <jack.chr.hensley@gmail.com>
2021-02-18 13:50:02 -0500

    added doc strings to least squares fitting functions

--- src/d02_extraction/extract_least_sq_fit.py
+++ src/d02_extraction/extract_least_sq_fit.py
@@ -1,13 +1,26 @@
 import numpy as np
 import pandas as pd
-from lmfit import minimize, Parameters, Parameter, report_fit
+from lmfit import minimize, Parameters, Parameter, report_fit, conf_interval, Minimizer
+import lmfit
+from sklearn.linear_model import LinearRegression
 
 from src.d00_utils.data_utils import save_data_frame, import_treated_csv_data
 from src.d03_modeling.model_functions import *
 
 
 def get_coefficients_with_lmfit(residuals_function, params, df, x_col_name, y_col_names):
-    """"""
+    """Fit parameters (params) with lmfit minimize package. Takes in data (df) as observations for constraining,
+    and residuals function (residuals_function) which calculates the residuals on params through constraining routine.
+    Residuals_function contains the required model functions that explain the params.
+
+    :param residuals_function:
+    :param params: Parameters. lmfit parameters object, in which each parameter corresponds to
+    a "coefficient" to be fit.
+    :param df: dataframe. Contains x_col_name (time values, usually) and y_col_names (corresponding variables).
+    :param x_col_name: str. Column name containing x variable. Usually column of times at which y values are measured.
+    :param y_col_names: List of str. Column names containing y variables. Must be ordered as written in model functions.
+    :return: results. dict? typical of lmfit, contains the fits and standard errors of fits.
+    """
 
     xs = df[x_col_name].values
     ys = []
@@ -15,8 +28,7 @@ def get_coefficients_with_lmfit(residuals_function, params, df, x_col_name, y_co
         ys.append(df[y_col].values)
     ys = np.asarray(ys)
 
-    results = minimize(residuals_function, params, args=(xs, ys), method='leastsq', nan_policy='omit')
-
+    results = minimize(residuals_function, params, args=(xs, ys), method='leastsq', nan_policy='raise')
     return results
 
 
@@ -30,16 +42,13 @@ def generate_coefficients(results_dict, N_runs=10000):
     """
 
     df_coefs = pd.DataFrame()
-    df_coef_statistics = pd.DataFrame()
-    df_coef_statistics['METRIC'] = ['avg', 'se']
 
     for key in results_dict.params:
         df_coefs[key] = np.random.normal(results_dict.params[key].value, results_dict.params[key].stderr, N_runs)
-        df_coef_statistics[key] = np.array([results_dict.params[key].value, results_dict.params[key].stderr])
 
     df_coefs = block_coefficients_outside_confidence_interval(df_coefs=df_coefs, ci=95)
 
-    return df_coefs, df_coef_statistics
+    return df_coefs
 
 
 def block_coefficients_outside_confidence_interval(df_coefs, ci=95):
@@ -60,3 +69,26 @@ def block_coefficients_outside_confidence_interval(df_coefs, ci=95):
     df_coefs_ci.reset_index(inplace=True)
 
     return df_coefs_ci
+
+
+def perform_regression(X, y):
+    """ Simple linear regression.
+    """
+
+    reg = LinearRegression().fit(X, y)
+    score = reg.score(X, y)
+
+    bs = reg.coef_
+
+    b0 = reg.intercept_
+
+    return b0, bs, score
+
+
+def report_characteristic_time(b1):
+    """ Calculates first order characteristic time from rate constant, k. """
+
+    b1 = float(b1)
+    tau = -1/b1
+
+    return tau
