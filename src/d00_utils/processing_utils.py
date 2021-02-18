36089643b189c39bd3d17018ff630e2cecc6d3d7
Jack Hensley <jack.chr.hensley@gmail.com>
2021-02-18 13:28:57 -0500

    moved individual processing functions to processing utils and added doc strings to all functions

--- src/d00_utils/processing_utils.py
+++ src/d00_utils/processing_utils.py
@@ -1,4 +1,8 @@
+import pandas as pd
 import numpy as np
+import statsmodels.api as sm
+
+from src.d00_utils.calc_utils import calculate_molarity_from_weight_fraction
 
 def normalize(unnormalized_array):
     """ Takes in an unnormalized array and normalizes it.
@@ -47,3 +51,165 @@ def get_bootstrapped_statistics(data):
     sample_rel_std = sample_std / sample_avg
 
     return sample_avg, sample_rel_std
+
+
+def add_estimated_nh_column(df, initial_composition, nh_source, compounds):
+    """ Estimates total ammonium (NH3 + NH4) from initial concentration and a 1:1 loss of butenedial. Adds
+    column with estimated NH concentration.
+
+    :param df: dataframe. Requires butenedial in a column.
+    :param initial_composition: dict. Contains the starting solution, with compound, wt-frac as key, value pairs.
+    :param nh_source: str. States the source of NH to the system. Used to ID molar ratio (e.g., ammonium sulfate -> 2).
+    :param compounds: dict. Contains dictionary of compounds in experiments. Required for any calibration.
+
+    :return df: dataframe. Dataframe now with column of total ammonium.
+    """
+
+    mw_nh = compounds[nh_source]['mw']
+    nh_shorthand_name = compounds[nh_source]['name']
+    mw_bd = compounds['butenedial']['mw']
+
+    nh_init = initial_composition[nh_shorthand_name]
+    bd_init = initial_composition['butenedial']  # assumes for now this is butenedial...
+
+    if nh_shorthand_name[0:4] == 'NH42':
+        N_nh = 2
+    else:
+        N_nh = 1
+
+    n_nh_init = N_nh * nh_init / mw_nh
+    n_bd_init = bd_init / mw_bd
+
+    molar_ratio = n_nh_init / n_bd_init
+
+    df['M_NH'] = molar_ratio * df['M_BUTENEDIAL']
+
+    return df
+
+
+def add_calibrated_data_column(df, calibration_params, compounds, initial_composition):
+    """ Adds calibrated columns to dataframe, calibrated according to specified method, from uncalibrated signal data.
+
+    :param df: dataframe. Requires butenedial in a column.
+    :param calibration_params: list of str. ID the calibration procedures to use.
+    :param initial_composition: dict. Contains the starting solution, with compound, wt-frac as key, value pairs.
+    :param compounds: dict. Contains dictionary of compounds in experiments. Required for any calibration.
+
+    :return df_with_calibrated_data: dataframe. Dataframe now with column of calibrated data.
+    """
+
+    df_with_calibrated_data = df
+
+    if 'calibration_query' in calibration_params.keys():
+        calibration_query = calibration_params['calibration_query']
+        df_calibrate = df.query(calibration_query)
+
+    else:
+        df_calibrate = df.copy(deep=True)
+
+    vars_to_calibrate = calibration_params['cols_to_calibrate']
+    N_calibrations = len(vars_to_calibrate)
+    for tick in range(N_calibrations):
+        var = vars_to_calibrate[tick]
+        method = calibration_params['method']
+
+        # uses the initial conditions to calculate the molarity (MS)
+        if method == 'weight_fraction_to_molarity':
+            analyte = calibration_params['analyte'][tick]
+            signal_avg = np.mean(df_calibrate[var])
+            molarity = calculate_molarity_from_weight_fraction(analyte=analyte,
+                                                               compounds=compounds,
+                                                               solution_comp=initial_composition)
+
+            calibration_factor = molarity / signal_avg
+
+        # uses the unchanging internal standard itself to calculate molarity (NMR)
+        if method == 'signal_to_molarity':
+            internal_standard = calibration_params['internal_standard']
+            internal_standard_shorthand = compounds[internal_standard]['name']
+            internal_standard_wt_frac = initial_composition[internal_standard_shorthand]
+            internal_standard_molecular_weight = compounds[internal_standard]['mw']
+            internal_standard_proton_count = compounds[internal_standard]['protons_in_nmr_peak']
+
+            calibration_factor = internal_standard_wt_frac * internal_standard_proton_count / \
+                                 internal_standard_molecular_weight
+
+        y_out_col = calibration_params['ys_out'][tick]
+        df_with_calibrated_data[y_out_col] = calibration_factor * df_with_calibrated_data[var]
+
+    return df_with_calibrated_data
+
+
+def add_estimated_ph_column(df, indicator):
+    """ Adds pH column to dataframe, according to chemical shift, in ppm, quantities in prescribed indicator column.
+
+    :param df: dataframe. Requires butenedial in a column.
+    :param indicator. str. Can either be acetic_acid, methyl_phosphonic_acid, or tri_methyl_phenol, and requires
+    the PPM_INTERNAL_STD or PPM_ACETIC_ACID column to be in the dataframe.
+
+    :return df_with_calibrated_data: dataframe. Dataframe now with column of estimated pH data
+    """
+
+    df_with_pH = df.copy(deep=True)
+
+    if indicator == 'acetic_acid':  # tynkkynen et al., 2009; wallace, 2018
+        shift_base = 1.900  # (wallace_shift_base: 1.906)
+        shift_acid = 2.096  # wallace_shift_acid: 2.0830
+        pka = 4.58  # wallace. 2018
+        df_with_pH['f_acid'] = (shift_base - df['PPM_ACETIC_ACID']) / (shift_base - shift_acid)
+        df_with_pH['f_base'] = 1 - df_with_pH['f_acid']
+
+    elif indicator == 'methyl_phosphonic_acid':  # wallace, 2015 (error < 0.1)
+        shift_base = 1.066  # wallace: shift_base: 1.0711
+        shift_acid = 1.294  # seen in low ph scenarios (wallace: shift_acid = 1.2819)
+        pka = 7.75
+
+        df_with_pH['f_acid'] = (shift_base - df['PPM_INTERNAL_STD']) / (shift_base - shift_acid)
+        df_with_pH['f_base'] = 1 - df_with_pH['f_acid']
+
+    elif indicator == 'tri_methyl_phenol':
+        shift_base = 6.355
+        shift_acid = 6.615
+        pka = 10.88
+        df_with_pH['f_acid'] = (shift_base - df['PPM_INTERNAL_STD']) / (shift_base - shift_acid)
+        df_with_pH['f_base'] = 1 - df_with_pH['f_acid']
+
+    df_with_pH['pH'] = pka + np.log10(df_with_pH['f_base'] / df_with_pH['f_acid'])
+    df_with_pH['H_PLUS'] = 10 ** (-df_with_pH['pH'])
+
+    return df_with_pH
+
+
+def add_normalized_intensity_column(df, internal_std_col='MZ283'):
+    """Add "MZ###_MZ###" columns to DataFrame of normalized peak intensities.
+
+    :param df: dataframe. Requires internal_std_col in a column.
+    :param internal_std_col: str. The title of column with internal standard used for normalizing the data.
+
+    :return df: dataframe. Dataframe now with normalized peak intensity data
+    """
+
+    mz_cols = [col for col in df.columns if col[:2] == 'MZ']  # first column of mass spec peaks is 'MZ'
+
+    for tick, mz_col in enumerate(mz_cols):
+        col_str = 'MZ' + mz_col[2:] + '_MZ' + internal_std_col[2:]
+        df[col_str] = df[mz_col] / df[internal_std_col]
+
+    return df
+
+
+def add_estimated_nh3_column(df, rh):
+    """Add gas-phase and droplet estimated ammonia concentrations based on concentration in bubbler and Henry's law.
+
+    :param df: dataframe. Requires mM_AMMONIA_BUBBLER in a column.
+    :param rh: float. The relative humidity in the EDB.
+
+    :return df_with_nh3: dataframe. Dataframe now with PPM_AMMONIA and M_AMMONIA columns.
+    """
+
+    H = 62
+    df_with_nh3 = df.copy(deep=True)
+    df_with_nh3['PPM_AMMONIA'] = df_with_nh3['mM_AMMONIA_BUBBLER'] * rh * (1 / H) * 1e3
+    df_with_nh3['M_AMMONIA'] = df_with_nh3['mM_AMMONIA_BUBBLER'] * 1e-3 * rh
+
+    return df_with_nh3
--- src/d01_data/process_ms_data.py
+++ src/d01_data/process_ms_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 
-from src.d00_utils.processing_utils import get_bootstrapped_statistics
-from src.d00_utils.calc_utils import calculate_molarity_from_weight_fraction
 from src.d00_utils.data_utils import *
+from src.d00_utils.processing_utils import *
 
 
 def process_data_for_experiments(experiments_dict, compounds, save_processed_data=False):
@@ -43,175 +43,69 @@ def process_data_for_experiments(experiments_dict, compounds, save_processed_dat
 
 def apply_processing_functions_in_experiment(df_unprocessed, experiments_dict, compounds,
                                              experiment, processing_function_strings):
-    """"""
+    """ Performs processing functions, which are listed in the processing_function_strings. For loop goes through
+    list of processing function strings and performs corresponding processing function. Nested within the
+    process_data_for_experiments function.
 
-    for function_string in processing_function_strings:
+    :param df_unprocessed: dataframe. Unprocessed input dataframe.
+    :param experiments_dict: dict. Contains the information necessary for the filtering and the paths.
+    :param compounds: dict. Contains dictionary of compounds in experiments. Required for any calibration.
+    :param experiment: str. Experiment within experiments_dict from which to extract processing function inputs.
+    :param processing_function_strings: list of str. Strings corresponding to function names in processing.
+    subdirectories of the treated_data directory.
+
+    :return df_processed: dataframe. Processed output dataframe.
+    """
+
+    for function_string in processing_function_strings:  # applies functions according to list from experiments yml
+
+        solution_weight_fractions = experiments_dict[experiment]['experimental_conditions']['solution_' +
+                                                                                            'weight_fractions']
 
         if function_string == 'normalize':
             df_unprocessed = add_normalized_intensity_column(df=df_unprocessed,
-                                                             internal_std_col='mz283')
+                                                             internal_std_col='MZ283')
+        if function_string == 'calculate_pH':
+            indicator = experiments_dict[experiment]['data_treatment']['pH_indicator']
+            df_unprocessed = add_estimated_ph_column(df=df_unprocessed,
+                                                     indicator=indicator)
 
         if function_string == 'calibrate_from_initial_conditions':
             calibration_params = experiments_dict[experiment]['data_treatment']['calibration_parameters']
-            solution_weight_fractions = experiments_dict[experiment]['experimental_conditions']['solution_' +
-                                                                                                'weight_fractions']
             df_unprocessed = add_calibrated_data_column(df=df_unprocessed,
                                                         calibration_params=calibration_params,
                                                         compounds=compounds,
                                                         initial_composition=solution_weight_fractions)
 
+        if function_string == 'calibrate_with_internal_standard':
+            calibration_params = experiments_dict[experiment]['data_treatment']['calibration_parameters']
+            df_unprocessed = add_calibrated_data_column(df=df_unprocessed,
+                                                        calibration_params=calibration_params,
+                                                        compounds=compounds,
+                                                        initial_composition=solution_weight_fractions)
+
+        if function_string == 'calculate_thca_nmer':
+            df_unprocessed['M_THCA_NMER'] = df_unprocessed['M_THCA'] - df_unprocessed['M_THCA_MONOMER']
+
+        if function_string == 'calculate_bd-pr-dz_nmer':
+            df_unprocessed['M_NMER'] = df_unprocessed['M_POLYMER'] - df_unprocessed['M_DIMER']
+
+        if function_string == 'calculate_nh':
+            nh_source = experiments_dict[experiment]['data_treatment']['nh_source']
+            df_unprocessed = add_estimated_nh_column(df=df_unprocessed,
+                                                     initial_composition=solution_weight_fractions,
+                                                     nh_source=nh_source,
+                                                     compounds=compounds)
+
+        if function_string == 'calculate_ammonia_from_bubbler':
+            df_unprocessed = add_estimated_nh3_column(df=df_unprocessed,
+                                                      rh=experiments_dict[experiment]['experimental_conditions'][
+                                                          'x_water'])
+
+        if function_string == 'merge_c4h5no_compounds':
+            df_unprocessed['M_C4H5NO'] = df_unprocessed['M_BUTENIMINE'] + df_unprocessed['M_PYRROLINONE']
+
+        if function_string == 'average_dimer':
+            df_unprocessed['M_DIMER'] = (df_unprocessed['M_DIMER_1'] + df_unprocessed['M_DIMER_2']) / 2
+
     return df_unprocessed
-
-
-def add_calibrated_data_column(df, calibration_params, compounds, initial_composition):
-    """"""
-
-    df_with_calibrated_data = df
-
-    calibration_query = calibration_params['calibration_query']
-    df_calibrate = df.query(calibration_query)
-
-    vars_to_calibrate = calibration_params['cols_to_calibrate']
-    N_calibrations = len(vars_to_calibrate)
-    for tick in range(N_calibrations):
-        var = vars_to_calibrate[tick]
-        analyte = calibration_params['analyte'][tick]
-        method = calibration_params['method']
-
-        if method == 'weight_fraction_to_molarity':
-            signal_avg = np.mean(df_calibrate[var])
-            molarity = calculate_molarity_from_weight_fraction(analyte=analyte,
-                                                               compounds=compounds,
-                                                               solution_comp=initial_composition)
-
-            calibration_factor = molarity / signal_avg
-
-        y_out_col = calibration_params['ys_out'][tick]
-        df_with_calibrated_data[y_out_col] = calibration_factor * df_with_calibrated_data[var]
-
-    return df_with_calibrated_data
-
-
-
-def process_ms_data_in_evap_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
-    """
-    """
-
-    experiment_name = [*experiments_dict].pop()
-    processing_params = experiments_dict[experiment_name]['processing']
-    composition = experiments_dict[experiment_name]['experimental']['composition']
-    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
-                                subdirectory=experiment_name)
-    df_processed = df_cleaned.copy(deep=True)
-
-
-    df_processed = df_processed.rename(columns={'mins': 'hrs'})
-    df_processed.hrs = df_processed.hrs / 60
-
-    if save_processed_data:
-        save_data_frame(df_to_save=df_processed,
-                        experiment_label=experiment_name,
-                        level_of_cleaning='PROCESSED')
-
-    return df_processed
-
-
-def process_ms_data_in_droplet_vs_vial_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
-    """
-    """
-
-    experiment_name = [*experiments_dict].pop()
-    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
-                                subdirectory=experiment_name)
-    df_processed = df_cleaned.copy(deep=True)
-    df_processed['hrs'] = (df_processed.mins + df_processed.vial) / 60
-    df_processed = df_processed.drop(['vial', 'mins'], axis=1)
-
-    if save_processed_data:
-        save_data_frame(df_to_save=df_processed,
-                        experiment_label=experiment_name,
-                        level_of_cleaning='PROCESSED')
-
-    return df_processed
-
-
-def process_ms_data_in_pyrrolinone_evap_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
-    """
-    """
-
-    experiment_name = [*experiments_dict].pop()
-    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
-                                subdirectory=experiment_name)
-    df_processed = df_cleaned.copy(deep=True)
-
-    df_processed = df_processed.rename(columns={'mins': 'hrs', 'vial': 'hrs_in_vial'})
-    df_processed.hrs = df_processed.hrs / 60
-    df_processed.hrs_in_vial = df_processed.hrs_in_vial / 60
-    df_processed['experiment'] = "pyr_nh42so4"
-    df_processed = df_processed[df_processed.hrs < 3]
-
-    if save_processed_data:
-        save_data_frame(df_to_save=df_processed,
-                        experiment_label=experiment_name,
-                        level_of_cleaning='PROCESSED')
-
-    return df_processed
-
-
-def process_ms_data_in_nh3g_experiments(cleaned_ms_file_name, experiments_dict, save_processed_data=False):
-    """
-    """
-    experiment_name = [*experiments_dict].pop()
-    df_cleaned = import_ms_data(file_name=cleaned_ms_file_name,
-                                subdirectory=experiment_name)
-    df_processed = df_cleaned.copy(deep=True)
-    df_processed['experiment'] = 'bd_nh3g_' + df_processed.nominal_nh3_mM.astype(str).str.replace('.','')
-
-    # df_processed['mM_nhx']  add this to the droplet definitions soon
-
-    if save_processed_data:
-        save_data_frame(df_to_save=df_processed,
-                        experiment_label=experiment_name,
-                        level_of_cleaning='PROCESSED')
-
-    return df_processed
-
-
-def add_calibrated_ms_data_columns(df, processing_params, composition, analyte, internal_standard='PEG-6'):
-    """"""
-
-    df_calibrated = df.copy()
-    ms_signal_inits = extract_calibration_data(df=df,
-                                               t_init_cutoff=processing_params['cal_data_time'],
-                                               cal_data_col=processing_params['y_col'])
-    ms_signal_inits_avg, ms_signal_inits_rel_std = get_bootstrapped_statistics(ms_signal_inits)
-
-    for compound_name, compound_mol_frac in composition.items():
-        internal_standard_mol_frac = composition[internal_standard]
-
-        if compound_name == analyte:
-            rel_molar_abundance_in_solution = compound_mol_frac / internal_standard_mol_frac
-            cal_factor_avg = rel_molar_abundance_in_solution / ms_signal_inits_avg
-            cal_factor_std = ms_signal_inits_rel_std
-
-            cal_data_col = processing_params['y_col'].replace('mz', 'mol')
-
-            df_calibrated[cal_data_col] = df_calibrated[processing_params['y_col']] * cal_factor_avg
-            df_calibrated[cal_data_col + '_std'] = df_calibrated[cal_data_col] * cal_factor_std
-
-            decay_data_col = cal_data_col.split('/')[0] + '/' + cal_data_col.split('/')[0] + '_0'
-            df_calibrated[decay_data_col] = df_calibrated[cal_data_col] / rel_molar_abundance_in_solution
-
-    return df_calibrated
-
-
-def add_normalized_intensity_column(df, internal_std_col='p283'):
-    """Add "mz###_mz###" columns to DataFrame of normalized peak intensities.
-    """
-
-    p_cols = [col for col in df.columns if col[0] == 'p']  # first column of mass spec peaks is p
-
-    for tick, p_col in enumerate(p_cols):
-        df['mz' + p_col[1:] + '_mz' + internal_std_col[1:]] = df[p_col] / df[internal_std_col]
-
-    return df
